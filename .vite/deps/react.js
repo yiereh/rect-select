1e18b901e8283f7daf90a2e1d5389e061b702c3c
TsukaharaHiroyuki <tsukahara@atrris.com>
2025-04-03 21:23:08 +0900

    first working commit

---
+++ .vite/deps/react.js
@@ -0,0 +1,4 @@
+import {
+  require_react
+} from "./chunk-UQKM4Z2Q.js";
+export default require_react();
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<div className="flex flex-col items-center justify-center mx-auto w-full h-screen">
+				<h1>Hello world</h1>
+			</div>
+		</>
+	);
+}
+
+export default App;
